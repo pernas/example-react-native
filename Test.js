ff35358f01cecb0bf1df2753b80bd7179749095a
Jaume Pernas <jaume@pernas.cat>
2017-03-16 17:31:17 +0000

    clean nativify files

--- Test.js
+++
@@ -1,7 +0,0 @@
-export const HELLO = 'HELLO_DUDE'
-// const BYE = 'BYE_DUDE'
-// export default BYE
-
-
-const foo = {baz: 42, bar: false}
-export default foo
--- wallet_example.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-global.self = global
-const network = require('dream-wallet/src/network')
-
-module.exports = function example() {
-  const api = network.createWalletApi()
-  const p = api.getWallet('f9df366a-3fc3-4826-827f-fb3c1e8ce616','00efae13-985b-4858-81ad-71bd8b5ac863','100 cent')
-  return p
-};
